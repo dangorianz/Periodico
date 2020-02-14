c1208f2ee2481f707085e2d2d7e4546dd107d1ab
Daniel Gorianz <aalbaafikaa@gmail.com>
2020-02-14 12:08:32 -0400

    se corrigio el bug que habia en las imagenes

--- js/llenarDeportes.js
+++ js/llenarDeportes.js
@@ -15,7 +15,7 @@ let direccionURLD = 'https://diarionuevonorte.herokuapp.com';
         <h6>${noticia.nombrecategoria}</h6>
         <img class="imgUltimaNoticia" src='../img/ultimaNoticia.png'>
         <a href="noticia.html">
-            <img src="${noticia.fotografia}" alt="">
+            <img class="imagenesUltimasCinco" src="${noticia.fotografia}" alt="">
             <h4>${noticia.titulo}</h4>
             <p class="quienPublico">por ${noticia.nombres} ${noticia.apellidos}</p>
         </a>
